49fbfba37963f8d6842faffde897ed12cd6e1cc9
MurphyL <im@murphyl.com>
2016-05-13 21:34:53 +0800

    增加layout文件

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,72 @@
-var snippets = {}, cats = [];
-var group = function (c, u) {
-    if (snippets.hasOwnProperty(c)) {
-        snippets[c].push(u);
-    } else {
-        cats.push({name: c});
-        snippets[c] = [u];
-    }
-};
 var init = function () {
-    /*var calcWindowSize = function () {
-        var mw = document.querySelector('body').offsetWidth - document.querySelector('#nav').offsetWidth - document.querySelector('#list').offsetWidth;
-        document.querySelector('#main').setAttribute('style', 'width:' + mw + 'px');
-    };
-    window.onresize = calcWindowSize;
-    calcWindowSize();*/
-    var xhq = new XMLHttpRequest();
+  // 数据
+  var snippets = {}, cats = [];
+  // 分组逻辑
+  var group = function (c, u) {
+      if (snippets.hasOwnProperty(c)) {
+          snippets[c].push(u);
+      } else {
+          cats.push({name: c});
+          snippets[c] = [u];
+      }
+  };
+  // 分组数据
+  Array.from(arguments).forEach(function(ele, idx){
+      ele && group(ele.category, ele.details, idx);
+  });
 
-    var get = function (url, callback) {
-        xhq.onreadystatechange = function () {
-            if (xhq.readyState == 4 && xhq.status == 200) {
-                //console.log(xhq.responseText)
-                callback.call(this, xhq.responseText);
-            }
-        }
-        xhq.open('GET', url, true);
-        xhq.send();
-    };
+  // ajax
+  var xhq = new XMLHttpRequest();
+  var get = function (url, callback) {
+      xhq.onreadystatechange = function () {
+          if (xhq.readyState == 4 && xhq.status == 200) {
+              callback.call(this, xhq.responseText);
+          }
+      }
+      xhq.open('GET', url, true);
+      xhq.send();
+  };
 
-    var list = new Vue({
-        el: '#list',
-        data: {
-            snippets: snippets[cats[0].name]
-        },
-        methods: {
-            show: function (snippet) {
-                renderWhiteBoard(snippet);
-                console.log(snippet);
-            }
-        }
-    });
-    var main;
-    var renderWhiteBoard = function (snippet) {
-        get(snippet.url, function (res) {
-            // console.log(res)
-            // document.querySelector('#whiteboard').innerHTML = res;
-            if(main){
-                main.$set('snippet', snippet);
-                main.$set('content', res);
-            } else {
-                main = new Vue({
-                    el: '#main',
-                    data: {
-                        snippet: snippet,
-                        content: res
-                    },
-                });
-            }
-        });
-    };
-    renderWhiteBoard(snippets[cats[0].name][0]);
-    new Vue({
-        el: '#cats',
-        data: {
-            cats: cats
-        },
-        methods: {
-            greet: function (key) {
-                // 方法内 `this` 指向 vm
-                list.snippets = snippets[key];
-                renderWhiteBoard(snippets[key][0]);
-            }
-        }
-    });
+  var list = new Vue({
+      el: '#list',
+      data: {
+          snippets: snippets[cats[0].name]
+      },
+      methods: {
+          show: function (snippet) {
+              renderWhiteBoard(snippet);
+              console.log(snippet);
+          }
+      }
+  });
+  var main;
+  var renderWhiteBoard = function (snippet) {
+      get(snippet.url, function (res) {
+          if(main){
+              main.$set('snippet', snippet);
+              main.$set('content', res);
+          } else {
+              main = new Vue({
+                  el: '#main',
+                  data: {
+                      snippet: snippet,
+                      content: res
+                  },
+              });
+          }
+      });
+  };
+  renderWhiteBoard(snippets[cats[0].name][0]);
+  new Vue({
+      el: '#cats',
+      data: {
+          cats: cats
+      },
+      methods: {
+          greet: function (key) {
+              list.snippets = snippets[key];
+              renderWhiteBoard(snippets[key][0]);
+          }
+      }
+  });
 }
